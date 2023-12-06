d9897f8bf95402a219a3042a6725189db2be0386
Diego Aldair García Hernández <147885940+DiegoAldair01@users.noreply.github.com>
2023-12-05 22:13:34 -0600

    Update2 main.c
    Recorre los primeros tres 'q', debemos hacer algo para moverse a la izquierda, apuntadores (?)

--- main.c
+++ main.c
@@ -1,29 +1,79 @@
-#include <stdio.h>
-#include <string.h>
-
 int main(){
-    int a;
-    int b;
+    int n;
+    int m;
     
 
     //Solicitar números a multiplicar a usuario
     printf("Ingrese el primer número: ");
-    scanf("%d", &a);
+    scanf("%d", &n);
     printf("Ingrese el segundo número: ");
-    scanf("%d", &b);
+    scanf("%d", &m);
 
 
     //Crear cintas del automata
-    int tamano1 = (a+b)+2;
-    int tamano2 = (a*b);
-    int cinta1[tamano1];
-    int cinta2[tamano2];
+    int tamano1 = (n+m)+1;
+    int tamano2 = (n*m);
+    char cinta1[tamano1];
+    char cinta2[tamano2];
+
+    //Llenar cinta 1 con la secuencia
+    memset(cinta1, 'a', n);
+    memset(cinta1+n, 'b', m);
+    cinta1[n+m] = 'c';
+    cinta1[n+m+1] = 'B';
+
+    // Imprimir contenido de la cinta 1
+    printf("Cinta 1: %s\n", cinta1);
+
+    int flag = 0;
+    int apuntador = 0;
+    //Recorrer cinta 1 y llenar cinta 2
+    for(int i = 0; i < tamano1; i++){
+        printf("Posición %d: %c\n", i, cinta1[i]);
+
+        //Verificar primer a
+        if(cinta1[i] == 'a' && flag == 0){
+            cinta2[i] = 'x';
+            flag = 1;
+            i++;
+        }
+        else if(cinta1[i] == 'a')
+        {
+            cinta1[i] = 'a';
+            i++;
+            while (cinta1[i] == 'a')
+            {
+                cinta1[i] = 'a';
+                i++;
+            }
+            
+        } 
+        else if(cinta1[i] == 'b'){
+            cinta1[i] = 'b';
+            cinta2[apuntador] = 'y';
+            i++;
+            apuntador++;
+            while (cinta1[i] == 'b')
+            {
+                cinta1[i] = 'b';
+                cinta2[apuntador] = 'y';
+                i++;
+                apuntador++;
+            }
+        }
+        else if(cinta1[i] == 'c'){
+            cinta1[i] = 'c';
+            //Debemos hacer algo para que vaya a la izquierda
+        }
+            
+        
+
+
+    }
 
 
     //Mostrar resultado
-    printf("El resultado de la multiplicación es: %d\n", a*b);
+    printf("El resultado de la multiplicación es: %d\n", n*m);
 
     return 0;
 }
-
-
