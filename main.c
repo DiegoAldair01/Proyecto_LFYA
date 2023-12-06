21eab4a116187e8a4119ed515adbbf4b6a03ebad
Diego Aldair García Hernández <147885940+DiegoAldair01@users.noreply.github.com>
2023-12-05 21:18:52 -0600

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,29 @@
-// Holaaa, aquí va el código del programa xd
-
 #include <stdio.h>
+#include <string.h>
 
 int main(){
-    printf("Hola mundo\n");
+    int a;
+    int b;
+    
+
+    //Solicitar números a multiplicar a usuario
+    printf("Ingrese el primer número: ");
+    scanf("%d", &a);
+    printf("Ingrese el segundo número: ");
+    scanf("%d", &b);
+
+
+    //Crear cintas del automata
+    int tamano1 = (a+b)+2;
+    int tamano2 = (a*b);
+    int cinta1[tamano1];
+    int cinta2[tamano2];
+
+
+    //Mostrar resultado
+    printf("El resultado de la multiplicación es: %d\n", a*b);
 
     return 0;
 }
-
 
 
